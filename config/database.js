f0965f6352993161f29716d5d0ce9b7960c4fa33
cbcslogistics <cbcslogistics@outlook.com>
2024-10-14 16:37:12 +0000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Create a new Sequelize instance connected to your MySQL database
-const sequelize = new Sequelize('sql5735241', 'sql5735241', 'mgEMPzvESc', {
+const sequelize = new Sequelize('sql5737786', 'sql5737786', 'aLG7eV8ieR', {
   host: 'sql5.freesqldatabase.com',
   dialect: 'mysql', // use 'mysql' for MySQL
   logging: false,   // Disable logging SQL queries, set to true for debugging
