43b7cd1969730b50cfcea10f05b832dd3d9422da
cbcslogistics <cbcslogistics@outlook.com>
2024-10-22 15:15:16 +0000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Create a new Sequelize instance connected to your MySQL database
-const sequelize = new Sequelize('cbcs57706534_tracking', 'web', 'Badman@2024', {
+const sequelize = new Sequelize('cbcs57706534_tracking', 'cbcs57706534_web', 'Badman@2024', {
   host: 'cp3.hostsilo.com',
   dialect: 'mysql', // use 'mysql' for MySQL
   logging: false,   // Disable logging SQL queries, set to true for debugging
